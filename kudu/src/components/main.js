1d80e7730c04d6a4b906b9e485b60b0b49ee3a00
Matt DeBoard <matt.deboard@gmail.com>
2015-07-06 19:33:32 -0500

    Factor out World object to separate module.

--- kudu/src/components/main.js
+++ kudu/src/components/main.js
@@ -1,22 +1,14 @@
 'use strict';
 
-var Immutable = require('immutable');
 var KuduApp = require('./KuduApp');
 var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
+var World = require('../World');
 var AR = require('../ARShim');
-var ClientLocation = require('./ClientLocation');
-
-var World = {
-  locationChanged: function(lat, lon, altitude, accuracy) {
-    ClientLocation.merge({ lat, lon, altitude });
-    React.render(<KuduApp lat={lat} lon={lon} altitude={altitude} />,
-                 document.getElementById('content'));
-  }
-};
 
 AR.context.onLocationChanged = World.locationChanged;
+World.fetchPOIs();
 React.render(<KuduApp />, document.getElementById('content'));
 
 /**
